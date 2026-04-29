1688db285774278ff40494dad8d2ca7ab1bef0d1
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-04-29 08:52:06 -0500

    include radix parser for int

--- Java/src/main/java/utils/Parse.java
+++ Java/src/main/java/utils/Parse.java
@@ -1,14 +1,24 @@
 package utils;
 import java.util.function.Function;
+import java.util.function.BiFunction;
 
 public class Parse {
     private static <T> T toT(String in, Function<String, T> parser) {
         return parser.apply(in);
     }
+    private static <T> T toT(String in, int radix, BiFunction<String, Integer, T> parser) {
+        return parser.apply(in, radix);
+    }
     public static int toInt(String str) {
-        return toT(str, Integer::parseInt);
+        return Parse.toInt(str, 10);
+    }
+    public static int toInt(String str, int radix) {
+        return toT(str, radix, Integer::parseInt);
     }
     public static float toFloat(String str) {
         return toT(str, Float::parseFloat);
+    }
+    public static double toDouble(String str) {
+        return toT(str, Double::parseDouble);
     }
 }
