3ecbd525d01d231aebe06280dbfe5009f65c27aa
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-07-15 15:04:37 -0500

    add round func

--- Java/src/main/java/utils/Utils.java
+++ Java/src/main/java/utils/Utils.java
@@ -1,4 +1,6 @@
 package utils;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 public class Utils {
     public static int count(String input, String target) {
         int res = 0;
@@ -31,5 +33,10 @@ public class Utils {
     }
     public static <T> boolean is(Class<T> clazz, Object obj) {
         return clazz.isInstance(obj);
+    }
+    public static double round(double in, int places) {
+        BigDecimal bd = BigDecimal.valueOf(in);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
